ee264d70b72b8f823a7a7f5d1b47f1b91babc9db
FORTELalejandro.montalvan <alejandro.montalvan@fortelcorp.com>
2025-11-20 02:35:14 -0500

    logout commit

---
+++ menu.php
@@ -0,0 +1,163 @@
+<div class="sidenav-menu">
+    <a href="index.php" class="logo">
+        <span class="logo-light">
+            <span class="logo-lg"><img src="./assets/images/logo.png" alt="logo"></span>
+            <span class="logo-sm text-center"><img src="./assets/images/logo-sm.png" alt="small logo"></span>
+        </span>
+
+        <span class="logo-dark">
+            <span class="logo-lg"><img src="./assets/images/logo-dark.png" alt="dark logo"></span>
+            <span class="logo-sm text-center"><img src="./assets/images/logo-sm.png" alt="small logo"></span>
+        </span>
+    </a>
+
+    <button class="button-sm-hover">
+        <i class="ti ti-circle align-middle"></i>
+    </button>
+
+    <button class="button-close-fullsidebar">
+        <i class="ti ti-x align-middle"></i>
+    </button>
+
+    <div data-simplebar>
+        <ul class="side-nav">
+
+            <li class="side-nav-item">
+                <a href="home.php" class="side-nav-link">
+                    <span class="menu-icon"><i class="ti ti-dashboard"></i></span>
+                    <span class="menu-text"> Dashboard </span>
+                </a>
+            </li>
+
+            <li class="side-nav-title mt-2">Users & Customers</li>
+
+            <li class="side-nav-item">
+                <a href="usuarios.php" class="side-nav-link">
+                    <span class="menu-icon"><i class="ti ti-message-filled"></i></span>
+                    <span class="menu-text"> Usuarios </span>
+                </a>
+            </li>
+
+            <li class="side-nav-item">
+                <a data-bs-toggle="collapse" href="#sidebarEcommerce" aria-expanded="false" aria-controls="sidebarEcommerce" class="side-nav-link">
+                    <span class="menu-icon"><i class="ti ti-basket-filled"></i></span>
+                    <span class="menu-text"> Productos </span>
+                    <span class="menu-arrow"></span>
+                </a>
+                <div class="collapse" id="sidebarEcommerce">
+                    <ul class="sub-menu">
+                        <li class="side-nav-item">
+                            <a href="apps-ecommerce-products.html" class="side-nav-link">
+                                <span class="menu-text">Products</span>
+                            </a>
+                        </li>
+                        <li class="side-nav-item">
+                            <a href="apps-ecommerce-products-grid.html" class="side-nav-link">
+                                <span class="menu-text">Products Grid</span>
+                            </a>
+                        </li>
+                        <li class="side-nav-item">
+                            <a href="apps-ecommerce-product-details.html" class="side-nav-link">
+                                <span class="menu-text">Product Details</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+
+            <li class="side-nav-title mt-2">Cotizaciones</li>
+
+            <li class="side-nav-item">
+                <a data-bs-toggle="collapse" href="#sidebarExtendedUI" aria-expanded="false" aria-controls="sidebarExtendedUI" class="side-nav-link">
+                    <span class="menu-icon"><i class="ti ti-alien-filled"></i></span>
+                    <span class="menu-text"> Extended UI </span>
+                    <span class="menu-arrow"></span>
+                </a>
+                <div class="collapse" id="sidebarExtendedUI">
+                    <ul class="sub-menu">
+                        <li class="side-nav-item">
+                            <a href="extended-dragula.html" class="side-nav-link">
+                                <span class="menu-text">Dragula</span>
+                            </a>
+                        </li>
+                        <li class="side-nav-item">
+                            <a href="extended-sweetalerts.html" class="side-nav-link">
+                                <span class="menu-text">Sweet Alerts</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+
+            <li class="side-nav-title mt-2">
+                More
+            </li>
+        
+            <li class="side-nav-item">
+                <a data-bs-toggle="collapse" href="#sidebarMultiLevel" aria-expanded="false" aria-controls="sidebarMultiLevel" class="side-nav-link">
+                    <span class="menu-icon"><i class="ti ti-box-multiple-3"></i></span>
+                    <span class="menu-text"> Multi Level </span>
+                    <span class="menu-arrow"></span>
+                </a>
+                <div class="collapse" id="sidebarMultiLevel">
+                    <ul class="sub-menu">
+                        <li class="side-nav-item">
+                            <a data-bs-toggle="collapse" href="#sidebarSecondLevel" aria-expanded="false" aria-controls="sidebarSecondLevel" class="side-nav-link">
+                                <span class="menu-text"> Second Level </span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <div class="collapse" id="sidebarSecondLevel">
+                                <ul class="sub-menu">
+                                    <li class="side-nav-item">
+                                        <a href="javascript: void(0);" class="side-nav-link">
+                                            <span class="menu-text">Item 1</span>
+                                        </a>
+                                    </li>
+                                    <li class="side-nav-item">
+                                        <a href="javascript: void(0);" class="side-nav-link">
+                                            <span class="menu-text">Item 2</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="side-nav-item">
+                            <a data-bs-toggle="collapse" href="#sidebarThirdLevel" aria-expanded="false" aria-controls="sidebarThirdLevel" class="side-nav-link">
+                                <span class="menu-text"> Third Level </span>
+                                <span class="menu-arrow"></span>
+                            </a>
+                            <div class="collapse" id="sidebarThirdLevel">
+                                <ul class="sub-menu">
+                                    <li class="side-nav-item">
+                                        <a href="javascript: void(0);" class="side-nav-link">Item 1</a>
+                                    </li>
+                                    <li class="side-nav-item">
+                                        <a data-bs-toggle="collapse" href="#sidebarFourthLevel" aria-expanded="false" aria-controls="sidebarFourthLevel" class="side-nav-link">
+                                            <span class="menu-text"> Item 2 </span>
+                                            <span class="menu-arrow"></span>
+                                        </a>
+                                        <div class="collapse" id="sidebarFourthLevel">
+                                            <ul class="sub-menu">
+                                                <li class="side-nav-item">
+                                                    <a href="javascript: void(0);" class="side-nav-link">
+                                                        <span class="menu-text">Item 2.1</span>
+                                                    </a>
+                                                </li>
+                                                <li class="side-nav-item">
+                                                    <a href="javascript: void(0);" class="side-nav-link">
+                                                        <span class="menu-text">Item 2.2</span>
+                                                    </a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+        <div class="clearfix"></div>
+    </div>
+</div>
